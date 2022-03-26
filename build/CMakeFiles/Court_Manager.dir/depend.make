7cfb4aafe99e13c9b7f81c636064d682590ec618
Hector <1518840850@qq.com>
2022-03-26 17:14:14 +0800

    delete .vscode and build file

--- build/CMakeFiles/Court_Manager.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Court_Manager.
-# This may be replaced when dependencies are built.
--- build/config.h
+++
@@ -1,7 +0,0 @@
-#ifndef CONFIGS_H
-#define CONFIGS_H
-
-#define  BASE_DIR                           "D:/Jupyter/c++_coding/yumaoqiu02/"
-#define TEXT_DIR                           "D:/Jupyter/c++_coding/yumaoqiu02/text/"
-
-#endif  // CONFIGS_H
